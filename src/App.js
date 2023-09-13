af4767b4d7c7ce79c7670bb2c195e9b33d0dea0f
ArtPere <artiom.perepecin@gmail.com>
2023-09-13 16:55:10 +0200

    We are here

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">here is begining</div>;
+  return <div className="App">second changes</div>;
 }
 
 export default App;
